494daa1cc0237bfb6d98eb65deed51fae2fe41ac
Goutam Prasad <goutam.prasad.2707@gmail.com>
2024-06-14 16:51:01 +0530

    The key design of the digital clock

---
+++ script.js
@@ -0,0 +1,15 @@
+let hrs = document.querySelector("#hrs");
+let min = document.querySelector("#min");
+let sec = document.querySelector("#sec");
+
+function clock() {
+	setInterval(() => {
+		let currentTime = new Date();
+
+		hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+		min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+		sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+	},1000);
+}
+
+clock();
